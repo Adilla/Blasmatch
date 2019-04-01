1aa51de82720dc75dcc4f8090098f3ad72b47c00
Adilla <adilla.susungi@gmail.com>
2019-04-01 17:46:09 +0200

    added files for pattern matching for blas kernels

--- test/inputs/gemm.c
+++ test/inputs/gemm.c
@@ -1,27 +1,22 @@
+double D[1024][1024];
+double C[1024][1024];
+double A[1024][1024];
+double B[1024][1024];
+double alpha, beta;
 
-float A[1024][1024];
-float B[1024][1024];
-float C[1024][1024];
-float alpha = 1.1;
-float beta = 1.1;
+void gemm() {
 
-//int ni = 1024;
-//int nj = 1024;
-//int nk = 1024;
-
-int main(void) {
+  for (int i = 0; i < 1024; i++)
+    for (int j = 0; j < 1024; j++) 
+        D[i][j] = beta * C[i][j];
 
 #pragma scop
-  /* C := alpha*A*B + beta*C */
-  for (int i = 0; i < 1024; i++)
-    for (int j = 0; j < 1024; j++)
-      {
-        C[i][j] *= beta;
-        for (int k = 0; k < 1024; ++k)
-          C[i][j] += alpha * A[i][k] * B[k][j];
+  for (int i = 0; i < 1024; i++) {
+    for (int j = 0; j < 1024; j++) { 
+      for (int k = 0; k < 1024; k++) { 
+            D[i][j] += alpha * A[i][k] * B[k][j];
       }
+    }
+  }
 #pragma endscop
-
-return 0;
-
 }
